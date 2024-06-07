b294bc229e7c89db3ee24d0dfc325a5833717ead
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-06-07 14:18:28 +0600

    End of the working week

---
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -0,0 +1,48 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class HabrCareerParse {
+
+    private static final String SOURCE_LINK = "https://career.habr.com";
+    public static final String PREFIX = "/vacancies?page=";
+    public static final String SUFFIX = "&q=Java%20developer&type=all";
+
+//    public static void main(String[] args) throws IOException {
+//        int pageNumber = 1;
+//        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+//        Connection connection = Jsoup.connect(fullLink);
+//        Document document = connection.get();
+//        Elements rows = document.select(".vacancy-card__inner");
+//        rows.forEach(row -> {
+//            Element titleElement = row.select(".vacancy-card__title").first();
+//            Element linkElement = titleElement.child(0);
+//            String vacancyName = titleElement.text();
+//            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+//            System.out.printf("%s %s%n", vacancyName, link);
+//        });
+//    }
+
+    public static void main(String[] args) throws IOException {
+        int pageNumber = 1;
+        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+        Connection connection = Jsoup.connect(fullLink);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element dateElement = row.select(".vacancy-card__date").first().child(0);
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            String vacancyName = titleElement.text();
+            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String date  = dateElement.text();
+            System.out.printf("%s %s %s%n", vacancyName, link, date);
+        });
+    }
+}
--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,16 +3,13 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.*;
 
 public class AlertRabbit {
@@ -33,6 +30,9 @@ public class AlertRabbit {
             var url = getProperties().getProperty("jdbc.url");
             var user = getProperties().getProperty("jdbc.username");
             var password = getProperties().getProperty("jdbc.password");
+            if (url == null || user == null || password == null) {
+                throw new SQLException("Database connection properties are not set correctly.");
+            }
             return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             throw new SQLException(e);
@@ -47,20 +47,29 @@ public class AlertRabbit {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDataMap jobDataMap = new JobDataMap();
+            Properties properties = getProperties();
+            String intervalStr = properties.getProperty("rabbit.interval");
+            if (intervalStr == null) {
+                throw new RuntimeException("Property 'time.interval' is not set in rabbit.properties.");
+            }
+            int interval = Integer.parseInt(intervalStr);
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
             jobDataMap.put("connection", connection);
             JobDetail job = newJob(Rabbit.class)
                     .usingJobData(jobDataMap)
                     .build();
             Trigger trigger = newTrigger()
                     .startNow()
+                    .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
             Thread.sleep(10000);
             scheduler.shutdown();
-            connection.close();
         } catch (SchedulerException se) {
             se.printStackTrace();
-        } catch (SQLException | InterruptedException e) {
+        } catch (SQLException | InterruptedException | IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -68,29 +77,30 @@ public class AlertRabbit {
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) {
-            try (Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection")) {
-                StringBuilder sql = new StringBuilder();
-                try (BufferedReader reader = new BufferedReader(
-                        new FileReader("src/main/resources/db/scripts/001_ddl_create_rabbit_table.sql")))  {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        sql.append(line).append("\n");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+            LocalDateTime currentTime = LocalDateTime.now();
+            String sql = "INSERT INTO rabbit (created_date) VALUES (?)";
+            Connection connection = null;
+            try {
+                Properties dbProps = (Properties) context.getJobDetail().getJobDataMap().get("dbProps");
+                String url = dbProps.getProperty("jdbc.url");
+                String user = dbProps.getProperty("jdbc.username");
+                String password = dbProps.getProperty("jdbc.password");
+                connection = DriverManager.getConnection(url, user, password);
+                try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                    ps.setObject(1, currentTime);
+                    ps.executeUpdate();
+                    System.out.println("Timestamp inserted successfully: " + currentTime);
                 }
-                try (Statement statement = connection.createStatement()) {
-                    String[] sqlCommands = sql.toString().split(";");
-                    for (String command : sqlCommands) {
-                        if (!command.trim().isEmpty()) {
-                            statement.execute(command);
-                        }
-                    }
-                } catch  (SQLException e)  {
-                    e.printStackTrace();
-                }
-            } catch  (SQLException e)  {
+            } catch (SQLException e) {
                 e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
