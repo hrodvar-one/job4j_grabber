09612c3c127741e272ab2900e169803dd8a2775b
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-06-17 13:11:13 +0600

    2.4. HabrCareerParse [#285213 #501019]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -7,6 +7,6 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 }
